b8db061563d51d0845c25eae8fb29d4c4a0f73a4
IbrahimSaid1 <squ118977@gmail.com>
2023-05-17 12:18:15 +0400

    Get Task in Employee

--- SalarySystem-main/src/main/java/com/codeline/sampleProject/Controller/EmployeeController.java
+++ SalarySystem-main/src/main/java/com/codeline/sampleProject/Controller/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.codeline.sampleProject.Controller;
 
 import com.codeline.sampleProject.Models.Employee;
+import com.codeline.sampleProject.RequestObjects.GetEmployeeRequestObject;
+import com.codeline.sampleProject.ResponseObjects.GetEmployeeResponse;
 import com.codeline.sampleProject.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,8 +19,8 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     @RequestMapping("employee/create")
-    public void saveEmployee () {
-        createEmployee();
+    public void saveEmployee (@RequestBody GetEmployeeRequestObject employeeRequestObject) {
+        createEmployee(employeeRequestObject);
     }
 
     @RequestMapping("employee/get")
@@ -24,15 +28,18 @@ public class EmployeeController {
         return employeeService.getEmployees();
     }
 
+    @RequestMapping("employee/get/{employeeId}")
+    public GetEmployeeResponse createEmployee (@PathVariable Long employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
 
-
-    public void createEmployee() {
+    public void createEmployee(GetEmployeeRequestObject employeeRequestObject) {
 
         Employee employee = new Employee();
-        employee.setName("Abdullah");
-        employee.setGender("Male");
+        employee.setName(employeeRequestObject.getName());
+        employee.setGender(employeeRequestObject.getGender());
         employee.setSalary(650.0);
-        employee.setDepartment("IT");
+        employee.setDepartment(employeeRequestObject.getDepartment());
         employee.setCompanyName("TechM");
         employee.setCreatedDate(new Date());
         employee.setIsActive(true);
