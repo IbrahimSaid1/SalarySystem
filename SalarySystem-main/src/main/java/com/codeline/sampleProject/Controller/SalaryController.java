2601c87f73c767ec07f81691c66d7d8d7f537a26
IbrahimSaid1 <squ118977@gmail.com>
2023-05-28 11:48:31 +0400

    Update SalaryController

--- SalarySystem-main/src/main/java/com/codeline/sampleProject/Controller/SalaryController.java
+++ SalarySystem-main/src/main/java/com/codeline/sampleProject/Controller/SalaryController.java
@@ -42,8 +42,6 @@ public class SalaryController {
 
 
 
-
-
     public void createSalary(GetSalaryRequestObject salaryRequestObject){
         Salary salary= new Salary();
         salary.setAmount(salaryRequestObject.getAmount());
