f150cdae2c8e35966118b5aa41841c01a5dce511
IbrahimSaid1 <squ118977@gmail.com>
2023-05-28 11:50:44 +0400

    Update AccountService

--- SalarySystem-main/src/main/java/com/codeline/sampleProject/Service/AccountService.java
+++ SalarySystem-main/src/main/java/com/codeline/sampleProject/Service/AccountService.java
@@ -40,6 +40,10 @@ public class AccountService {
 
 
 
+
+
+
+
     public GetAccountResponse getAccountAsString(Account account) {
         GetAccountResponse accountResponse = new GetAccountResponse(account.getBankName(), account.getAccountNumber(), account.getBankBranch());
         return accountResponse;
