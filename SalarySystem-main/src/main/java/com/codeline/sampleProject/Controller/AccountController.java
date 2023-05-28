8bf858c129d336c80342320a0a6b4318910dd264
IbrahimSaid1 <squ118977@gmail.com>
2023-05-28 11:43:41 +0400

    Update AccountController

--- SalarySystem-main/src/main/java/com/codeline/sampleProject/Controller/AccountController.java
+++ SalarySystem-main/src/main/java/com/codeline/sampleProject/Controller/AccountController.java
@@ -44,6 +44,11 @@ public class AccountController {
     }
 
 
+
+
+
+
+
     @RequestMapping("account/delete/{Id}")
     public void deleteaccount (@PathVariable Long Id) {
         accountService.deleteAccountById(Id);
